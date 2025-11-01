b9183919a6eb9205ae74d05d422ccb9d57464275
Alex <ax.zaverukha@gmail.com>
2025-11-01 19:11:49 +0200

    add new styles for header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,10 +5,12 @@ import styles from './Layout.module.css'
 const Layout = ({ children, currentPage, onPageChange }) => {
   return (
     <div className={styles.layout}>
-      <Header currentPage={currentPage} onPageChange={onPageChange} />
-      <main className={styles.mainContent}>
-        {children}
-      </main>
+      <div className={styles.contentContainer}>
+        <Header currentPage={currentPage} onPageChange={onPageChange} />
+        <main className={styles.mainContent}>
+          {children}
+        </main>
+      </div>
       <ScrollToTop />
     </div>
   )
